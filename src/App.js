760dd2a50b393ab2f8ec14343bdbaf3852a6583a
lochungtin <lochungtin@gmail.com>
2022-09-17 21:04:47 +0100

    basic clickable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Background from './svg/background';
+import Interactable from './svg/interactable';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='stack'>
+				<Background />
+				<Interactable />
+			</div>
+		</div>
+	);
 }
 
 export default App;
