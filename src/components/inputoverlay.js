f73a227925d4a227d5e32080c6af236fff58b5c4
lochungtin <lochungtin@gmail.com>
2022-09-19 15:13:30 +0100

    larger

--- src/components/inputoverlay.js
+++ src/components/inputoverlay.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function InputOverlay() {
-	return <div className='inputOverlayBg'></div>;
+	return (
+		<div className='inputOverlayBg'>
+			<div className='inputBg'></div>
+		</div>
+	);
 }
